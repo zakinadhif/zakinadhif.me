18d95d798fb753facb1a6d75c036d14c29aa1e20
Zaki Nadhif <zaki.nadhif45@gmail.com>
2022-07-21 15:14:17 +0700

    Minor change on components

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function Footer() {
     return (
-        <footer className="text-xs text-smoke py-10 flex justify-between">
+        <footer className="text-sm text-smoke py-10 flex justify-between">
             <div>Inspired By <a href="http://peterforgacs.github.io/" className="text-bluemoon underline">Peter Forgacs</a>' notes.txt</div>
-            <nav className="font-thin text-xs tracking-wider text-smoke">
-                <ul className="flex divide-x divide-smoke divide-dotted underline">
+            <nav className="font-thin tracking-wider text-smoke">
+                <ul className="flex divide-x divide-smoke divide-dotted underline underline-offset-1">
                     <li className="px-3 pl-0"><Link href="/home"><a>Home</a></Link></li>
                     <li className="px-3"><Link href="/about"><a>About</a></Link></li>
                     <li className="px-3"><Link href="/writing"><a>Writing</a></Link></li>
