75b312fbd19e7229db4eb458a563b79606963c55
Zaki Nadhif <zaki.nadhif45@gmail.com>
2022-07-23 18:32:02 +0700

    Minor change on footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div>Inspired By <a href="http://peterforgacs.github.io/" className="text-bluemoon underline">Peter Forgacs</a>' notes.txt</div>
             <nav className="font-thin tracking-wider text-smoke">
                 <ul className="flex divide-x divide-smoke divide-dotted underline underline-offset-1">
-                    <li className="px-3 pl-0"><Link href="/home"><a>Home</a></Link></li>
+                    <li className="px-3 pl-0"><Link href="/"><a>Home</a></Link></li>
                     <li className="px-3"><Link href="/about"><a>About</a></Link></li>
                     <li className="px-3"><Link href="/writing"><a>Writing</a></Link></li>
                     <li className="px-3 pr-0"><Link href="/projects"><a>Projects</a></Link></li>
