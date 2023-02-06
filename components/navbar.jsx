9080c2ba13a075510612c263bb5795a10bc4f78f
domain121314 <telkomdev.domain@gmail.com>
2023-02-06 10:10:25 +0700

    Minor style changes

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,19 +3,17 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <div className="flex h-16 mb-3 md:mb-0 md:h-32 relative gap-4 md:gap-7 items-center">
-                <Image src="/assets/code.svg" width={74} height={41} />
-            <div className="flex flex-col justify-center items-center md:items-start grow">
-                <div className="font-bold text-3xl">zaki_nadhif.txt</div>
-                <nav className="font-light text-sm tracking-widest text-bluemoon hidden md:block">
-                    <ul className="flex divide-x divide-bluemoon divide-dotted">
-                        <li className="px-6 pl-0"><Link href="/"><a className="hover:underline underline-offset-1">Home</a></Link></li>
-                        <li className="px-6"><Link href="/about"><a className="hover:underline underline-offset-1">About</a></Link></li>
-                        <li className="px-6"><Link href="/archives"><a className="hover:underline underline-offset-1">Writing</a></Link></li>
-                        <li className="px-6 pr-0"><a href="https://github.com/zakinadhif" className="hover:underline underline-offset-1">Projects</a></li>
-                    </ul>
-                </nav>
-            </div>
+        <div className="relative py-12">
+            <div className="bg-[url('/assets/code.svg')] w-[78px] h-[55px] bg-contain bg-no-repeat bg-center float-left inline-block mr-6"/>
+            <div className="font-bold text-2xl">zakinadhif.my.id</div>
+            <nav className="font-light text-sm tracking-widest text-bluemoon hidden md:block">
+                <ul className="flex divide-x divide-bluemoon divide-dotted">
+                    <li className="px-4 pl-0"><Link href="/"><a className="hover:underline underline-offset-1">Home</a></Link></li>
+                    <li className="px-4"><Link href="/about"><a className="hover:underline underline-offset-1">About</a></Link></li>
+                    <li className="px-4"><Link href="/archives"><a className="hover:underline underline-offset-1">Writing</a></Link></li>
+                    <li className="px-4 pr-0"><a href="https://github.com/zakinadhif" className="hover:underline underline-offset-1">Projects</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -22,11 +22,11 @@ export default function Home({ allPostsData }) {
         <title>Zaki Nadhif's Notes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p className="mb-8">
+      <p className="mb-9">
         I'm Zaki Nadhif, a junior software developer from Indonesia. Currently a
         twelfth grader at SMK Telkom Malang.
       </p>
-      <h2 className="text-2xl text-bluemoon mb-4 font-medium">Writing</h2>
+      <h2 className="text-2xl text-bluemoon mb-4 font-medium flex items-center gap-4"><i className="ri-book-open-line font-medium" />Writing</h2>
       <ul className="space-y-3 md:space-y-1 font-medium">
         {allPostsData.map(({ slugStr, date, title }) => (
           <li className="flex md:flex-row flex-col" key={slugStr}>
@@ -35,7 +35,7 @@ export default function Home({ allPostsData }) {
           </li>
         ))}
       </ul>
-      <h2 className="text-2xl text-bluemoon mb-4 font-medium mt-7">Projects</h2>
+      <h2 className="text-2xl text-bluemoon mb-4 font-medium mt-7 flex items-center gap-4"><i class="ri-code-s-slash-line font-medium" />Projects</h2>
       <ul className="space-y-1 font-medium">
         <li><a className="underline underline-offset-1" href="https://github.com/cowdingus/SpaceShooter/tree/engine">Egienx</a>: A toy C++ game engine with Lua Scripting.</li>
         <li><a className="underline underline-offset-1" href="https://example.com">Smart Garden</a>: Backyard garden automation with Arduino Uno.</li>
