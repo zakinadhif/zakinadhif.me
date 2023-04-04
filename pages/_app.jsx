7565ba9abc891eb69aa4719304ab6c03cf7a4506
Zaki Nadhif <zaki.nadhif45@gmail.com>
2023-04-04 11:30:01 +0700

    Add 'theme-color' meta tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,20 @@
-import '../styles/globals.css'
-import '../styles/blog.scss'
-import 'remixicon/fonts/remixicon.css'
-import 'highlight.js/styles/night-owl.css'
+import "../styles/globals.css";
+import "../styles/blog.scss";
+import "remixicon/fonts/remixicon.css";
+import "highlight.js/styles/night-owl.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name='theme-color' content='#161A26' />
+        <meta name='msapplication-navbutton-color' content='#161A26' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='#161A26' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
